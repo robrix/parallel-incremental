85aaf9230d5633aac817732704962ba0947b6236
Rob Rix <rob.rix@me.com>
2017-11-14 15:30:01 -0500

    Define a Semigroup instance for M.

--- src/Data/Sparse/Matrix.hs
+++ src/Data/Sparse/Matrix.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds, FlexibleInstances, GADTs, StandaloneDeriving #-}
 module Data.Sparse.Matrix where
 
+import Data.Semigroup
 import Data.Sparse.Shape
 import qualified Data.Sparse.Vector as V
 
@@ -37,3 +38,14 @@ r a11 a12 = R a11 a12
 c :: M 'L y1 a -> M 'L y2 a -> M 'L ('B y1 y2) a
 c Z Z = Z
 c a11 a21 = C a11 a21
+
+
+instance (Eq a, Monoid a, Semigroup a) => Semigroup (M x y a) where
+  Z <> x = x
+  x <> Z = x
+  O x <> O y = o (x <> y)
+  R a11 a12 <> R b11 b12 = r (a11 <> b11) (a12 <> b12)
+  C a11 a21 <> C b11 b21 = c (a11 <> b11) (a21 <> b21)
+  Q a11 a12 a21 a22 <> Q b11 b12 b21 b22
+    = q (a11 <> b11) (a12 <> b12)
+        (a21 <> b21) (a22 <> b22)
