160c44ada3dfb0c37c5e782bb6b2c616a524d18c
Rob Rix <rob.rix@me.com>
2017-11-14 16:10:48 -0500

    Rename the singleton matrix constructor to L.

--- src/Data/Sparse/Matrix.hs
+++ src/Data/Sparse/Matrix.hs
@@ -3,17 +3,17 @@ module Data.Sparse.Matrix where
 
 import Data.Semigroup
 import Data.Semiring
-import Data.Sparse.Shape
+import qualified Data.Sparse.Shape as S
 import qualified Data.Sparse.Vector as V
 
 data M x y a where
   Z :: M x y a
-  O :: a -> M 'L 'L a
-  R :: M x1 'L a -> M x2 'L a -> M ('B x1 x2) 'L a
-  C :: M 'L y1 a -> M 'L y2 a -> M 'L ('B y1 y2) a
+  L :: a -> M 'S.L 'S.L a
+  R :: M x1 'S.L a -> M x2 'S.L a -> M ('S.B x1 x2) 'S.L a
+  C :: M 'S.L y1 a -> M 'S.L y2 a -> M 'S.L ('S.B y1 y2) a
   Q :: M x1 y1 a -> M x2 y1 a
     -> M x1 y2 a -> M x2 y2 a
-    -> M ('B x1 x2) ('B y1 y2) a
+    -> M ('S.B x1 x2) ('S.B y1 y2) a
 
 deriving instance Eq a   => Eq   (M x y a)
 deriving instance Show a => Show (M x y a)
@@ -26,36 +26,36 @@ deriving instance Show a => Show (SomeM a)
 z :: M x y a
 z = Z
 
-o :: (Eq a, Monoid a) => a -> M 'L 'L a
-o x | x == zero = Z
-    | otherwise = O x
+l :: (Eq a, Monoid a) => a -> M 'S.L 'S.L a
+l x | x == zero = Z
+    | otherwise = L x
 
-q0 :: M ('B x1 x2) ('B y1 y2) a
+q0 :: M ('S.B x1 x2) ('S.B y1 y2) a
 q0 = Q Z Z Z Z
 
-q :: M x1 y1 a -> M x2 y1 a -> M x1 y2 a -> M x2 y2 a -> M ('B x1 x2) ('B y1 y2) a
+q :: M x1 y1 a -> M x2 y1 a -> M x1 y2 a -> M x2 y2 a -> M ('S.B x1 x2) ('S.B y1 y2) a
 q Z Z Z Z = Z
 q a11 a12 a21 a22 = Q a11 a12 a21 a22
 
-r :: M x1 'L a -> M x2 'L a -> M ('B x1 x2) 'L a
+r :: M x1 'S.L a -> M x2 'S.L a -> M ('S.B x1 x2) 'S.L a
 r Z Z = Z
 r a11 a12 = R a11 a12
 
-c :: M 'L y1 a -> M 'L y2 a -> M 'L ('B y1 y2) a
+c :: M 'S.L y1 a -> M 'S.L y2 a -> M 'S.L ('S.B y1 y2) a
 c Z Z = Z
 c a11 a21 = C a11 a21
 
 
 fromVec :: V.V s a -> M s s a
 fromVec V.Z = Z
-fromVec (V.L a) = O a
+fromVec (V.L a) = L a
 fromVec (V.B v1 v2) = q (fromVec v1) Z Z (fromVec v2)
 
 
 instance (Eq a, Monoid a, Semigroup a) => Semigroup (M x y a) where
   Z <> x = x
   x <> Z = x
-  O x <> O y = o (x <> y)
+  L x <> L y = l (x <> y)
   R a11 a12 <> R b11 b12 = r (a11 <> b11) (a12 <> b12)
   C a11 a21 <> C b11 b21 = c (a11 <> b11) (a21 <> b21)
   Q a11 a12 a21 a22 <> Q b11 b12 b21 b22
@@ -71,9 +71,9 @@ infixr 7 `mult`
 mult :: (Eq a, Semiring a) => M x y a -> M z x a -> M z y a
 Z `mult` _ = Z
 _ `mult` Z = Z
-O x `mult` O y = o (x >< y)
-O x `mult` R a11 a12 = r (O x `mult` a11) (O x `mult` a12)
-C a11 a21 `mult` O x = c (a11 `mult` O x) (a21 `mult` O x)
+L x `mult` L y = l (x >< y)
+L x `mult` R a11 a12 = r (L x `mult` a11) (L x `mult` a12)
+C a11 a21 `mult` L x = c (a11 `mult` L x) (a21 `mult` L x)
 R a11 a12 `mult` C b11 b21 = a11 `mult` b11 <> a12 `mult` b21
 C a11 a21 `mult` R b11 b12
   = q (a11 `mult` b11) (a11 `mult` b12)
