73622dd5c6ae26f507509aa62ec3699f81372a8f
Rob Rix <rob.rix@me.com>
2017-11-26 05:50:13 -0500

    Define Functor & Applicative instances for Grammar.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -1,8 +1,17 @@
 {-# LANGUAGE GADTs #-}
 module Data.Grammar where
 
+import Control.Applicative
+
 data Grammar s a where
   Nul :: a -> Grammar s a
   Lit :: s -> Grammar s s
   Alt :: Grammar s a -> Grammar s a -> Grammar s a
   Seq :: (a -> b -> c) -> Grammar s a -> Grammar s b -> Grammar s c
+
+instance Functor (Grammar s) where
+  fmap = liftA
+
+instance Applicative (Grammar s) where
+  pure = Nul
+  liftA2 = Seq
