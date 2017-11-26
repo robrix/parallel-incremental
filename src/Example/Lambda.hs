42404cc716a60b82aceee0eba7de369c5cb71556
Rob Rix <rob.rix@me.com>
2017-11-26 14:14:27 -0500

    Define a name production.

--- src/Example/Lambda.hs
+++ src/Example/Lambda.hs
@@ -1,4 +1,11 @@
 module Example.Lambda where
 
+import Control.Applicative
+import Text.Parser.Char
+import Text.Parser.Token
+
 data Lam = Abs String Lam | App Lam Lam | V String
   deriving (Eq, Show)
+
+name :: TokenParsing m => m String
+name = token ((:) <$> letter <*> many alphaNum)
