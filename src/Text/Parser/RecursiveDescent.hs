507c3b2f1825e206f4022ce45df5ed9db8b7ed2a
Rob Rix <rob.rix@me.com>
2017-11-26 13:34:49 -0500

    Define a helper to format the expectation for an error.

--- src/Text/Parser/RecursiveDescent.hs
+++ src/Text/Parser/RecursiveDescent.hs
@@ -1,4 +1,12 @@
 module Text.Parser.RecursiveDescent where
 
+import Data.List (intercalate)
+
 type State s = [s]
 type Error s = ([String], State s)
+
+formatExpectation :: [String] -> String
+formatExpectation [] = "eof"
+formatExpectation [e1] = e1
+formatExpectation [e1, e2] = e1 ++ " or " ++ e2
+formatExpectation es = intercalate ", " (init es) ++ ", or " ++ last es
