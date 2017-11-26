53c588f9e1230191bd1c85792e1fa4c95745898b
Rob Rix <rob.rix@me.com>
2017-11-26 05:49:43 -0500

    Add a nullary rule to Grammar.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -2,6 +2,7 @@
 module Data.Grammar where
 
 data Grammar s a where
+  Nul :: a -> Grammar s a
   Lit :: s -> Grammar s s
   Alt :: Grammar s a -> Grammar s a -> Grammar s a
   Seq :: (a -> b -> c) -> Grammar s a -> Grammar s b -> Grammar s c
