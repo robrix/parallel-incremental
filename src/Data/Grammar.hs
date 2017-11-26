1de96c083d3eb1974e55d0740be625a42cca8f93
Rob Rix <rob.rix@me.com>
2017-11-26 05:42:35 -0500

    Stub in a grammar datatype.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE GADTs #-}
 module Data.Grammar where
+
+data Grammar s a where
+  Lit :: s -> Grammar s s
+  Alt :: Grammar s a -> Grammar s a -> Grammar s a
+  Seq :: Grammar s a -> Grammar s b -> Grammar s (a, b)
