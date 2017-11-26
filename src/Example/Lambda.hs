7b01330a232cf7f8448796517d87fa0da0236bf9
Rob Rix <rob.rix@me.com>
2017-11-26 14:17:17 -0500

    Define a variable production.

--- src/Example/Lambda.hs
+++ src/Example/Lambda.hs
@@ -2,10 +2,14 @@ module Example.Lambda where
 
 import Control.Applicative
 import Text.Parser.Char
+import Text.Parser.Combinators
 import Text.Parser.Token
 
 data Lam = Abs String Lam | App Lam Lam | Var String
   deriving (Eq, Show)
+
+var :: TokenParsing m => m Lam
+var = Var <$> name <?> "variable"
 
 name :: TokenParsing m => m String
 name = token ((:) <$> letter <*> many alphaNum)
