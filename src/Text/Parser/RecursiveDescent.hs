3036ec9d1338d7f02abaa3c341e6579c08748070
Rob Rix <rob.rix@me.com>
2017-11-26 13:35:06 -0500

    Define a helper to format an error.

--- src/Text/Parser/RecursiveDescent.hs
+++ src/Text/Parser/RecursiveDescent.hs
@@ -5,6 +5,12 @@ import Data.List (intercalate)
 type State s = [s]
 type Error s = ([String], State s)
 
+formatError :: Show s => Error s -> String
+formatError ([], []) = "no rule to match at eof"
+formatError ([], cs) = "no rule to match at " ++ show cs
+formatError (es, []) = "expected " ++ formatExpectation es ++ " at eof"
+formatError (es, cs) = "expected " ++ formatExpectation es ++ " at " ++ show cs
+
 formatExpectation :: [String] -> String
 formatExpectation [] = "eof"
 formatExpectation [e1] = e1
