eb81dde6e441ae645eb9c9a52457de3c3b7aa809
Rob Rix <rob.rix@me.com>
2017-11-26 06:22:43 -0500

    Add a type parameter for the nonterminals.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -3,20 +3,20 @@ module Data.Grammar where
 
 import Control.Applicative
 
-data Grammar s a where
-  Empty :: Grammar s a
-  Nul :: a -> Grammar s a
-  Lit :: s -> Grammar s s
-  Alt :: Grammar s a -> Grammar s a -> Grammar s a
-  Seq :: (a -> b -> c) -> Grammar s a -> Grammar s b -> Grammar s c
+data Grammar s n a where
+  Empty :: Grammar s n a
+  Nul :: a -> Grammar s n a
+  Lit :: s -> Grammar s n s
+  Alt :: Grammar s n a -> Grammar s n a -> Grammar s n a
+  Seq :: (a -> b -> c) -> Grammar s n a -> Grammar s n b -> Grammar s n c
 
-instance Functor (Grammar s) where
+instance Functor (Grammar s n) where
   fmap = liftA
 
-instance Applicative (Grammar s) where
+instance Applicative (Grammar s m) where
   pure = Nul
   liftA2 = Seq
 
-instance Alternative (Grammar s) where
+instance Alternative (Grammar s n) where
   empty = Empty
   (<|>) = Alt
