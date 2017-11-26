a1089a4c9584a5737a60af0737584d979bbd782a
Rob Rix <rob.rix@me.com>
2017-11-26 05:43:03 -0500

    The Seq constructor takes a function.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -4,4 +4,4 @@ module Data.Grammar where
 data Grammar s a where
   Lit :: s -> Grammar s s
   Alt :: Grammar s a -> Grammar s a -> Grammar s a
-  Seq :: Grammar s a -> Grammar s b -> Grammar s (a, b)
+  Seq :: (a -> b -> c) -> Grammar s a -> Grammar s b -> Grammar s c
