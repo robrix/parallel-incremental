86ad06a3b2ea099128e559289c8d977ac83f919c
Rob Rix <rob.rix@me.com>
2017-11-27 10:52:54 -0500

    Define an Embed typeclass.

--- src/Data/Recursive.hs
+++ src/Data/Recursive.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Data.Recursive
 ( Recursive(..)
+, Embed(..)
 ) where
 
 class Recursive m where
   mu :: (m a -> m a) -> m a
+
+class Embed f t | t -> f where
+  embed :: f t a -> t a
