c4ce2f19e4a6cb360196c540d4805fee1dcb0f9b
Rob Rix <rob.rix@me.com>
2017-11-27 09:02:40 -0500

    Generalize iterRec over the result of the algebra.

--- src/Text/Parser/RecursiveDescent.hs
+++ src/Text/Parser/RecursiveDescent.hs
@@ -11,46 +11,45 @@ type Error t = ([String], State t)
 
 runGrammar :: (Eq t, Show t) => (forall n . Rec (Grammar t) n a) -> State t -> Either String a
 runGrammar grammar cs = first formatError $ do
-  (a, cs) <- iterRec algebra grammar cs
+  (a, cs) <- runK (iterRec algebra grammar) cs
   if null cs then
     Right a
   else
     Left  (["eof"], cs)
-  where algebra :: (Eq t, Show t) => (forall a . r a -> State t -> Either (Error t) (a, State t)) -> Grammar t r a -> State t -> Either (Error t) (a, State t)
-        algebra go g cs = case g of
+  where algebra :: (Eq t, Show t) => (forall a . r a -> K t a) -> Grammar t r a -> K t a
+        algebra go g = K $ \ cs -> case g of
           Err es -> Left (es, cs)
           Nul a -> Right (a, cs)
           Sat p | c:cs' <- cs, p c -> Right (c, cs')
                 | otherwise        -> Left  ([], cs)
-          Alt a b -> either (\ (e1, _) -> first (\ (e2, cs) -> (e1 ++ e2, cs)) (go b cs)) Right (go a cs)
+          Alt a b -> either (\ (e1, _) -> first (\ (e2, cs) -> (e1 ++ e2, cs)) (runK (go b) cs)) Right (runK (go a) cs)
           Seq f a b -> do
-            (a', cs')  <- go a cs
+            (a', cs')  <- runK (go a) cs
             let fa = f a'
-            (b', cs'') <- fa `seq` go b cs'
+            (b', cs'') <- fa `seq` runK (go b) cs'
             let fab = fa b'
             fab `seq` Right (fab, cs'')
-          Lab a s -> first (\ (_, cs) -> ([s], cs)) (go a cs)
+          Lab a s -> first (\ (_, cs) -> ([s], cs)) (runK (go a) cs)
           End | [] <- cs  -> Right ((), [])
               | otherwise -> Left  (["eof"], cs)
 
-iterRec :: forall a t
+newtype K t a = K { runK :: State t -> Either (Error t) (a, State t) }
+
+iterRec :: forall a t b
         .  (  forall a r
-           .  (forall a . r a -> State t -> Either (Error t) (a, State t))
+           .  (forall a . r a -> b a)
            -> Grammar t r a
-           -> State t
-           -> Either (Error t) (a, State t)
+           -> b a
            )
         -> (forall n . Rec (Grammar t) n a)
-        -> State t
-        -> Either (Error t) (a, State t)
+        -> b a
 iterRec algebra = go mempty
-  where go :: Env t
+  where go :: Env b
            -> Rec (Grammar t) Name r
-           -> State t
-           -> Either (Error t) (r, State t)
-        go env (In g) cs = algebra (go env) g cs
-        go env (Var v) cs = (env ! v) cs
-        go env (Rec r) cs = let (name, env') = extend (go env (Rec r)) env in go env' (r name) cs
+           -> b r
+        go env (In g) = algebra (go env) g
+        go env (Var v) = env ! v
+        go env (Rec r) = let (name, env') = extend (go env (Rec r)) env in go env' (r name)
 
 formatError :: Show t => Error t -> String
 formatError ([], []) = "no rule to match at eof"
@@ -67,20 +66,20 @@ formatExpectation es = intercalate ", " (init es) ++ ", or " ++ last es
 newtype Env t = Env { getEnv :: [Binding t] }
   deriving (Monoid)
 
-data Binding t where
-  Binding :: (State t -> Either (Error t) (a, State t)) -> Binding t
+data Binding b where
+  Binding :: b a -> Binding b
 
 newtype Name a = Name { getName :: Int }
   deriving (Eq, Show)
 
-(!) :: Env t -> Name a -> State t -> Either (Error t) (a, State t)
-(Env env ! Name n) s = go env (pred (length env)) n s
-  where go (Binding b : env) n n' s
-          | n == n'   = unsafeCoerce (b s)
-          | otherwise = go env (pred n) n' s
-        go [] _ n' _ = error ("(!): " ++ show n' ++ " out of bounds")
+(!) :: Env b -> Name a -> b a
+Env env ! Name n = go env (pred (length env)) n
+  where go (Binding b : env) n n'
+          | n == n'   = unsafeCoerce b
+          | otherwise = go env (pred n) n'
+        go [] _ n' = error ("(!): " ++ show n' ++ " out of bounds")
 
 infixl 0 !
 
-extend :: (State t -> Either (Error t) (a, State t)) -> Env t -> (Name a, Env t)
+extend :: b a -> Env b -> (Name a, Env b)
 extend cont (Env bs) = (Name (length bs), Env (Binding cont : bs))
