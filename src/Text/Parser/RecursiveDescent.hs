6fa99925061bd113b9569fcd658da408b8391d7c
Rob Rix <rob.rix@me.com>
2017-11-27 08:55:59 -0500

    Factor the iteration over Rec out of recursive descent parsing.

--- src/Text/Parser/RecursiveDescent.hs
+++ src/Text/Parser/RecursiveDescent.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, RankNTypes #-}
+{-# LANGUAGE GADTs, GeneralizedNewtypeDeriving, RankNTypes, ScopedTypeVariables #-}
 module Text.Parser.RecursiveDescent where
 
 import Data.Bifunctor
@@ -11,27 +11,44 @@ type Error t = ([String], State t)
 
 runGrammar :: (Eq t, Show t) => (forall n . Rec (Grammar t) n a) -> State t -> Either String a
 runGrammar grammar cs = first formatError $ do
-  (a, cs) <- go mempty grammar cs
+  (a, cs) <- iterRec algebra grammar cs
   if null cs then
     Right a
   else
     Left  (["eof"], cs)
-  where go :: (Eq t, Show t) => Env t -> Rec (Grammar t) Name a -> State t -> Either (Error t) (a, State t)
-        go env (In g) cs = case g of
+  where algebra :: (Eq t, Show t) => (forall a . r a -> State t -> Either (Error t) (a, State t)) -> Grammar t r a -> State t -> Either (Error t) (a, State t)
+        algebra go g cs = case g of
           Err es -> Left (es, cs)
           Nul a -> Right (a, cs)
           Sat p | c:cs' <- cs, p c -> Right (c, cs')
                 | otherwise        -> Left  ([], cs)
-          Alt a b -> either (\ (e1, _) -> first (\ (e2, cs) -> (e1 ++ e2, cs)) (go env b cs)) Right (go env a cs)
+          Alt a b -> either (\ (e1, _) -> first (\ (e2, cs) -> (e1 ++ e2, cs)) (go b cs)) Right (go a cs)
           Seq f a b -> do
-            (a', cs')  <- go env a cs
+            (a', cs')  <- go a cs
             let fa = f a'
-            (b', cs'') <- fa `seq` go env b cs'
+            (b', cs'') <- fa `seq` go b cs'
             let fab = fa b'
             fab `seq` Right (fab, cs'')
-          Lab a s -> first (\ (_, cs) -> ([s], cs)) (go env a cs)
+          Lab a s -> first (\ (_, cs) -> ([s], cs)) (go a cs)
           End | [] <- cs  -> Right ((), [])
               | otherwise -> Left  (["eof"], cs)
+
+iterRec :: forall a t
+        .  (  forall a r
+           .  (forall a . r a -> State t -> Either (Error t) (a, State t))
+           -> Grammar t r a
+           -> State t
+           -> Either (Error t) (a, State t)
+           )
+        -> (forall n . Rec (Grammar t) n a)
+        -> State t
+        -> Either (Error t) (a, State t)
+iterRec algebra = go mempty
+  where go :: Env t
+           -> Rec (Grammar t) Name r
+           -> State t
+           -> Either (Error t) (r, State t)
+        go env (In g) cs = algebra (go env) g cs
         go env (Var v) cs = (env ! v) cs
         go env (Rec r) cs = let (name, env') = extend (go env (Rec r)) env in go env' (r name) cs
 
