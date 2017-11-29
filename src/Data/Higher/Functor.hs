30829375610c87de98301e1d6c42d3c9a3efa106
Rob Rix <rob.rix@me.com>
2017-11-28 22:43:22 -0500

    Add <$ to higher-order Functors.

--- src/Data/Higher/Functor.hs
+++ src/Data/Higher/Functor.hs
@@ -5,7 +5,9 @@ module Data.Higher.Functor
 ) where
 
 import Data.Higher.Function as F
-import Prelude hiding (Functor)
+import Prelude hiding (Functor(..))
 
 class Functor f where
   fmap :: (a ~> b) -> f a ~> f b
+  (<$) :: (forall x . b x) -> f a ~> f b
+  a <$ r = fmap (const a) r
