a8e5e901502d38196f220a78d5a36cd81cad9c65
Rob Rix <rob.rix@me.com>
2017-11-28 22:35:03 -0500

    Define a lifting of first-order monoids to higher-order monoids.

--- src/Data/Higher/Monoid.hs
+++ src/Data/Higher/Monoid.hs
@@ -1,8 +1,19 @@
 module Data.Higher.Monoid
-( Data.Higher.Monoid.Monoid(..)
+( Monoid(..)
+, Lift(..)
 ) where
 
-import Data.Higher.Semigroup
+import Data.Higher.Semigroup (Semigroup(..))
+import qualified Data.Monoid as M
+import Prelude hiding (Monoid)
 
 class Semigroup m => Monoid m where
   mempty :: m a
+
+newtype Lift m a = Lift { lower :: m }
+
+instance M.Monoid m => Semigroup (Lift m) where
+  Lift a <> Lift b = Lift (a `M.mappend` b)
+
+instance M.Monoid m => Monoid (Lift m) where
+  mempty = Lift M.mempty
