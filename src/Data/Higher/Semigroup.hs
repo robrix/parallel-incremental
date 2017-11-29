20f92e52c6978870fad5f04fb8485bab59bf0ff5
Rob Rix <rob.rix@me.com>
2017-11-28 22:32:09 -0500

    Define a lifting of first-order semigroups to higher-order semigroups.

--- src/Data/Higher/Semigroup.hs
+++ src/Data/Higher/Semigroup.hs
@@ -1,4 +1,12 @@
 module Data.Higher.Semigroup where
 
+import qualified Data.Semigroup as S
+
 class Semigroup s where
   (<>) :: s a -> s a -> s a
+
+
+newtype Lift m a = Lift { lower :: m }
+
+instance S.Semigroup s => Semigroup (Lift s) where
+  Lift a <> Lift b = Lift (a S.<> b)
