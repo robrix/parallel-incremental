21635a92dbdf1c4aae44f6ba5272d0b8d5bded78
Rob Rix <rob.rix@me.com>
2017-11-28 22:29:16 -0500

    Export Monoid without hiding Prelude’s definition.

--- src/Data/Higher/Monoid.hs
+++ src/Data/Higher/Monoid.hs
@@ -1,9 +1,8 @@
 module Data.Higher.Monoid
-( Monoid(..)
+( Data.Higher.Monoid.Monoid(..)
 ) where
 
 import Data.Higher.Semigroup
-import Prelude hiding (Monoid(..))
 
 class Semigroup m => Monoid m where
   mempty :: m a
