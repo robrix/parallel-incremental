0389b33551f2905f54a4936713731044cb5a19e9
Rob Rix <rob.rix@me.com>
2017-11-29 10:05:13 -0500

    Define an expression type.

--- src/Example/Arithmetic.hs
+++ src/Example/Arithmetic.hs
@@ -1,1 +1,12 @@
 module Example.Arithmetic where
+
+data Expr a
+  = K a
+  | Add (Expr a) (Expr a)
+  | Mul (Expr a) (Expr a)
+  | Sub (Expr a) (Expr a)
+  | Div (Expr a) (Expr a)
+  | Exp (Expr a) (Expr a)
+  | Abs (Expr a)
+  | Sig (Expr a)
+  deriving (Eq, Show)
