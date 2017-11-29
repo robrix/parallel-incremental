a51704fcea62ebbf8bcf5e38f0eb343cc29a6cda
Rob Rix <rob.rix@me.com>
2017-11-28 22:13:22 -0500

    Define catamorphisms over Fix.

--- src/Data/Fix.hs
+++ src/Data/Fix.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Fix where
 
+import Data.Higher.Functor
 import Data.Function (fix)
 import Data.Recursive
 
 newtype Fix f a = Fix { unFix :: f (Fix f) a }
+
+hcata :: HFunctor f => (f a ~> a) -> Fix f ~> a
+hcata alg = alg . hfmap (hcata alg) . unFix
 
 instance Recursive (Fix f) where
   mu = fix
