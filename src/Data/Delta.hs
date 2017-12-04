758f2e7c3bee172688edcbd2a306ce08eb2cd248
Rob Rix <rob.rix@me.com>
2017-12-04 18:26:52 -0500

    Define an Ord instance for Delta.

--- src/Data/Delta.hs
+++ src/Data/Delta.hs
@@ -14,6 +14,9 @@ data Delta
 instance Eq Delta where
   (==) = (==) `on` deltaBytes
 
+instance Ord Delta where
+  compare = compare `on` deltaBytes
+
 instance Semigroup Delta where
   Delta l1 c1 b1 <> Delta l2 c2 b2 = Delta (l1 + l2) (addColumns c1 c2) (b1 + b2)
     where addColumns c1 c2 | l1 == 0 || l2 == 0 = c1 + c2
