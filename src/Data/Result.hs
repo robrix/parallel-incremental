5b8f08e0af668376a26afb6c26a30adb45083b23
Rob Rix <rob.rix@me.com>
2017-12-04 15:50:34 -0500

    Define an Applicative instance for Result.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -5,3 +5,10 @@ data Result e a
   = Failure [e]
   | Success a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Applicative (Result e) where
+  pure = Success
+
+  Success f  <*> a          = f <$> a
+  Failure e1 <*> Failure e2 = Failure (e1 ++ e2)
+  Failure e  <*> _          = Failure e
