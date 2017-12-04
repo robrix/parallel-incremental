6c1fc64ebcb51b2c92440c095d5457602931520a
Rob Rix <rob.rix@me.com>
2017-12-04 15:54:05 -0500

    Define an Align instance for Result.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -2,6 +2,8 @@
 module Data.Result where
 
 import Control.Applicative
+import Data.Align
+import Data.These
 
 data Result e a
   = Failure [e]
@@ -21,3 +23,11 @@ instance Alternative (Result e) where
   Failure e1 <|> Failure e2 = Failure (e1 ++ e2)
   Success a  <|> _          = Success a
   _          <|> Success b  = Success b
+
+instance Align (Result e) where
+  nil = empty
+
+  alignWith f (Success a) (Success b) = Success (f (These a b))
+  alignWith f (Success a) _           = Success (f (This  a))
+  alignWith f _           (Success b) = Success (f (That    b))
+  alignWith _ (Failure a) (Failure b) = Failure (a ++ b)
