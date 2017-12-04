a1b5f46b58279b6de1426ed0b6959378feeb194d
Rob Rix <rob.rix@me.com>
2017-12-04 15:48:12 -0500

    Derive aa bunch of instances for Result.

--- src/Data/Result.hs
+++ src/Data/Result.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
 module Data.Result where
 
 data Result e a
   = Failure [e]
   | Success a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
