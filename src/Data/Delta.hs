4752df7d265032018bbe9995e45a9b145a324674
Rob Rix <rob.rix@me.com>
2017-12-04 18:17:05 -0500

    Define a datatype for deltas.

--- src/Data/Delta.hs
+++ src/Data/Delta.hs
@@ -1,1 +1,8 @@
 module Data.Delta where
+
+data Delta
+  = Delta
+    { deltaLines   :: {-# UNPACK #-} !Int
+    , deltaColumns :: {-# UNPACK #-} !Int
+    , deltaBytes   :: {-# UNPACK #-} !Int
+    }
