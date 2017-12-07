c12e17e19dda8d2437c9f0f523aecc9be3ab7d42
Rob Rix <rob.rix@me.com>
2017-12-07 16:25:08 -0700

    Stub in a printing evaluator.

--- src/Text/Printer/RecursiveAscent.hs
+++ src/Text/Printer/RecursiveAscent.hs
@@ -1,1 +1,10 @@
-module Text.Printer.RecursiveAscent where
+{-# LANGUAGE RankNTypes #-}
+module Text.Printer.RecursiveAscent
+( runGrammar
+) where
+
+import Data.Grammar
+import Data.Rec
+
+runGrammar :: Show t => (forall n . Rec n (Grammar t) a) -> a -> [t]
+runGrammar _ _ = []
