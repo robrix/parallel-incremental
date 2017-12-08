17edeb25186a4e9f0367f725c85b23f9f4fc3c1c
Rob Rix <rob.rix@me.com>
2017-12-08 09:08:42 -0700

    Define a Cogrammar datatype.

--- src/Text/Printer/RecursiveAscent.hs
+++ src/Text/Printer/RecursiveAscent.hs
@@ -1,10 +1,19 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
 module Text.Printer.RecursiveAscent
 ( runGrammar
 ) where
 
-import Data.Grammar
 import Data.Rec
+import Data.These
 
-runGrammar :: Show t => (forall n . Rec n (Grammar t) a) -> a -> [t]
+data Cogrammar t r a
+  = Err [String]
+  | Nul a
+  | Sat (t -> Maybe a)
+  | forall c b . Alt (a -> These c  b)  (r c) (r b)
+  | forall c b . Seq (a ->      (c, b)) (r c) (r b)
+  | Lab (r a) String
+  | End a
+
+runGrammar :: Show t => (forall n . Rec n (Cogrammar t) a) -> a -> [t]
 runGrammar _ _ = []
