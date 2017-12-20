faf70d4459dc4916053cf032a15f4c38c5d9c3f2
Rob Rix <rob.rix@me.com>
2017-12-19 22:19:42 -0500

    Define a CofreeF datatype.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,3 +1,5 @@
 module Control.Higher.Comonad.Cofree where
 
 data Cofree f a x = Cofree { extract :: a x, unwrap :: f (Cofree f a) x }
+
+data CofreeF f a b x = CofreeF { headF :: a x, tailF :: f b x }
