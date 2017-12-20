d1afa46a7122c895bcd1855a9052df88d36918bd
Rob Rix <rob.rix@me.com>
2017-12-20 10:35:07 -0500

    Add a traverse method to Traversable.

--- src/Data/Higher/Traversable.hs
+++ src/Data/Higher/Traversable.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Traversable
 ( Traversable(..)
 , module F
 ) where
 
+import Control.Higher.Applicative as H
 import Data.Higher.Foldable as H
 import Data.Higher.Function as F
 import Prelude hiding (Traversable(..))
 
-class H.Foldable f => Traversable f where
+class H.Foldable t => Traversable t where
+  traverse :: H.Applicative f => (a ~> f b) -> t a ~> f (t b)
