fb99ad532400de4098126dfdc310ab66a33d36a8
Rob Rix <rob.rix@me.com>
2017-12-19 21:48:17 -0500

    Define Graph, Vertex, & Edge datatypes.

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -1,1 +1,7 @@
 module Data.Graph where
+
+data Graph a = Graph { graphName :: Maybe String, graphVertices :: [Vertex a], graphEdges :: [Edge] }
+
+data Vertex a = Vertex { vertexIdentifier :: Int, vertexValue :: Maybe a }
+
+data Edge = Edge { edgeFrom :: Int, edgeTo :: Int }
