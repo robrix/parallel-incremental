cb14387d0e2b2bfe91165b18b154adae01a1b8ec
Rob Rix <rob.rix@me.com>
2017-12-19 22:23:04 -0500

    Define a H.Functor instance for CofreeF.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE PolyKinds #-}
 module Control.Higher.Comonad.Cofree where
 
+import Data.Higher.Functor as H
+
 data Cofree f a x = Cofree { extract :: a x, unwrap :: f (Cofree f a) x }
 
 data CofreeF f a b x = CofreeF { headF :: a x, tailF :: f b x }
+
+instance H.Functor f => H.Functor (CofreeF f a) where
+  fmap f (CofreeF h t) = CofreeF h (H.fmap f t)
