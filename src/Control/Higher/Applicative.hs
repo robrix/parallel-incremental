a09eac0c23d2877660012abdf6f0408d0f4987d7
Rob Rix <rob.rix@me.com>
2017-12-20 10:27:41 -0500

    Stub in a higher-rank Applicative typeclass.

--- src/Control/Higher/Applicative.hs
+++ src/Control/Higher/Applicative.hs
@@ -1,1 +1,9 @@
-module Control.Higher.Applicative where
+module Control.Higher.Applicative
+( Applicative(..)
+, module F
+) where
+
+import Data.Higher.Function as F
+import Prelude hiding (Applicative(..))
+
+class Applicative f where
