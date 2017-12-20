ef42fcd9fd80e49eac7d85bc41b97f8b39f97934
Rob Rix <rob.rix@me.com>
2017-12-19 21:51:04 -0500

    Stub in a function to render a grammar to a graph.

--- src/Data/Grammar.hs
+++ src/Data/Grammar.hs
@@ -5,11 +5,13 @@ module Data.Grammar
 
 import Control.Applicative
 import Control.Monad (guard)
+import Data.Graph
 import qualified Data.Higher.Foldable as H
 import qualified Data.Higher.Functor as H
 import qualified Data.Higher.Monoid as H
 import Data.Higher.Functor.Classes as H
 import Data.Higher.Functor.Foldable
+import Data.Rec
 import Data.These
 import Text.Parser.Char
 import Text.Parser.Combinators
@@ -23,6 +25,10 @@ data Grammar t r a
   | forall c b . Seq (      c -> b -> a) (r c) (r b)
   | Lab (r a) String
   | End a
+
+toGraph :: Rec n (Grammar t) a -> Graph a
+toGraph _ = Graph Nothing [] []
+
 
 instance (Bounded t, Enum t, Show t) => H.Show1 (Grammar t) where
   liftShowsPrec sp d g = case g of
