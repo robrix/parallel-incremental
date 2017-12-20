01747a2c3f773cb306d0bf2a748526695a46a5f2
Rob Rix <rob.rix@me.com>
2017-12-20 10:36:18 -0500

    Add Functor as a superclass of Traversable.

--- src/Data/Higher/Traversable.hs
+++ src/Data/Higher/Traversable.hs
@@ -7,9 +7,10 @@ module Data.Higher.Traversable
 import Control.Higher.Applicative as H
 import Data.Higher.Foldable as H
 import Data.Higher.Function as F
+import Data.Higher.Functor as H
 import Prelude hiding (Traversable(..))
 
-class H.Foldable t => Traversable t where
+class (H.Foldable t, H.Functor t) => Traversable t where
   traverse :: H.Applicative f => (a ~> f b) -> t a ~> f (t b)
 
   sequenceA :: H.Applicative f => t (f a) ~> f (t a)
