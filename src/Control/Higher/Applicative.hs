73ba3be739ae2d01439e64d74dc6519b75f6abfc
Rob Rix <rob.rix@me.com>
2017-12-20 10:30:53 -0500

    Add Functor as a superclass of Applicative.

--- src/Control/Higher/Applicative.hs
+++ src/Control/Higher/Applicative.hs
@@ -4,10 +4,11 @@ module Control.Higher.Applicative
 , module F
 ) where
 
+import Data.Higher.Functor as H
 import Data.Higher.Function as F
 import Prelude hiding (Applicative(..))
 
-class Applicative f where
+class H.Functor f => Applicative f where
   pure :: a ~> f a
 
   (<*>) :: f (a :-> b) x -> f a x -> f b x
