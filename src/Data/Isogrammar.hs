2e830f6ecd4e28405c02b2ba880a8c261c3a3b62
Rob Rix <rob.rix@me.com>
2018-01-12 15:16:13 -0500

    Give the pretty-printer something to pretty-print.

--- src/Data/Isogrammar.hs
+++ src/Data/Isogrammar.hs
@@ -18,14 +18,14 @@ data Isogrammar t r a
   | End a
 
 
-prettyPrint :: (forall n . Rec n (Isogrammar Char) a) -> Maybe String
-prettyPrint grammar = toS <$> runK (iterRec algebra grammar)
+prettyPrint :: (forall n . Rec n (Isogrammar Char) a) -> a -> Maybe String
+prettyPrint grammar a = toS <$> runK (iterRec algebra grammar) a
   where algebra :: (r ~> K) -> Isogrammar Char r ~> K
-        algebra yield g = K $ case g of
+        algebra yield g = K $ \ a -> case g of
           Err _ -> Nothing
           _ -> Just mempty
 
-newtype K a = K { runK :: Maybe StringS }
+newtype K a = K { runK :: a -> Maybe StringS }
 
 newtype StringS = StringS { unStringS :: String -> String }
 
