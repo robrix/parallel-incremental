b4c0aefe22536f9e8db4042df04565c16bf70add
Rob Rix <rob.rix@me.com>
2018-01-12 11:29:39 -0500

    Define Seq over pairs.

--- src/Data/Isogrammar.hs
+++ src/Data/Isogrammar.hs
@@ -9,8 +9,8 @@ data Isogrammar t r a
   = Err [String]
   | Nul a
   | Sat (t -> Maybe a)
-  | forall c b . Alt (These c    b -> a) (r c) (r b)
-  | forall c b . Seq (      c -> b -> a) (r c) (r b)
+  | forall c b . Alt (These  c  b  -> a) (r c) (r b)
+  | forall c b . Seq (      (c, b) -> a) (r c) (r b)
   | Lab (r a) String
   | End a
 
