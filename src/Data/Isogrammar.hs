8bdd93a4e6adf20082bcfc672abe57693ecb38cf
Rob Rix <rob.rix@me.com>
2018-01-12 11:26:52 -0500

    Define an Isogrammar datatype.

--- src/Data/Isogrammar.hs
+++ src/Data/Isogrammar.hs
@@ -1,1 +1,13 @@
+{-# LANGUAGE ExistentialQuantification #-}
 module Data.Isogrammar where
+
+import Data.These
+
+data Isogrammar t r a
+  = Err [String]
+  | Nul a
+  | Sat (t -> Maybe a)
+  | forall c b . Alt (These c    b -> a) (r c) (r b)
+  | forall c b . Seq (      c -> b -> a) (r c) (r b)
+  | Lab (r a) String
+  | End a
