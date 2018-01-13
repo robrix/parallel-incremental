c6bd1ba1693e815443b8126a8b379220d5ed39aa
Rob Rix <rob.rix@me.com>
2018-01-13 15:12:47 -0500

    Define a fromList constructor for DList.

--- src/Data/DList.hs
+++ src/Data/DList.hs
@@ -4,6 +4,9 @@ import Data.Semigroup
 
 newtype DList a = DList { unDList :: [a] -> [a] }
 
+fromList :: [a] -> DList a
+fromList as = DList (as ++)
+
 instance Semigroup (DList a) where
   DList a <> DList b = DList (a . b)
 
