4974291d3d8d5f3242da96115cc68672079892a2
Rob Rix <rob.rix@me.com>
2018-01-20 15:34:26 -0500

    Define a Size typeclass operable on type-level shapes.

--- src/Data/Sparse/Shape.hs
+++ src/Data/Sparse/Shape.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes, DataKinds, KindSignatures, ScopedTypeVariables, TypeApplications #-}
 module Data.Sparse.Shape where
 
 import Data.Semigroup
@@ -8,3 +9,13 @@ data Shape = L | B Shape Shape
 
 instance Semigroup Shape where
   (<>) = B
+
+
+class Size (s :: Shape) where
+  size :: Num n => n
+
+instance Size 'L where
+  size = 1
+
+instance (Size s1, Size s2) => Size ('B s1 s2) where
+  size = size @s1 + size @s2
