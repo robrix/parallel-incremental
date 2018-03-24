f84de517df1ed309474f469a3fc7f7ee11f6acd7
Rob Rix <rob.rix@me.com>
2018-03-24 10:40:01 -0400

    Define a membership test.

--- src/Data/Relation.hs
+++ src/Data/Relation.hs
@@ -4,9 +4,11 @@ module Data.Relation
 , fromRelation
 , fromPredicate
 , singleton
+, lookup
 ) where
 
 import Control.Monad
+import Prelude hiding (lookup)
 
 newtype Relation i a = Relation (i -> Maybe a)
   deriving (Functor)
@@ -19,3 +21,6 @@ fromPredicate predicate = Relation (\ i -> guard (predicate i) *> pure i)
 
 singleton :: Eq i => i -> a -> Relation i a
 singleton i a = Relation ((*> pure a) . guard . (== i))
+
+lookup :: i -> Relation i a -> Maybe a
+lookup i (Relation m) = m i
