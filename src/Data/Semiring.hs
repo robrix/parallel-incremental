6923e73c1997f862d2356fd71a64284add2e5ee6
Rob Rix <rob.rix@me.com>
2018-03-24 10:17:46 -0400

    Define the Mult semigroup.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, DeriveGeneric, GeneralizedNewtypeDeriving #-}
 module Data.Semiring
 ( Semiring(..)
 , Semigroup(..)
+, Mult(..)
 ) where
 
 import Data.Semigroup
+import GHC.Generics
 
 class (Semigroup a, Monoid a) => Semiring a where
   zero :: a
@@ -12,3 +15,10 @@ class (Semigroup a, Monoid a) => Semiring a where
   (><) :: a -> a -> a
 
   infixr 7 ><
+
+
+newtype Mult a = Mult { getMult :: a }
+  deriving (Eq, Foldable, Functor, Generic, Generic1, Ord, Show, Traversable)
+
+instance Semiring a => Semigroup (Mult a) where
+  Mult a <> Mult b = Mult (a >< b)
