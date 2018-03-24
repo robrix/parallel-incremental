82459ffd0a474f362609f9db71bb9d8e805acaa2
Rob Rix <rob.rix@me.com>
2018-03-24 11:04:17 -0400

    Rename Production to Symbol.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -5,13 +5,13 @@ import Data.Monoid
 
 data TwoNF t = U t | B (TwoNF t) (TwoNF t)
 
-data CFG t n = CFG { start :: n, rules :: [(n, [Production t n])] }
+data CFG t n = CFG { start :: n, rules :: [(n, [Symbol t n])] }
 
 size :: Num a => CFG t n -> a
 size = getSum . foldMap (fromIntegral . length . snd) . rules
 
-data Production t n = T t | N n
+data Symbol t n = T t | N n
 
-instance Bifunctor Production where
+instance Bifunctor Symbol where
   bimap f _ (T t) = T (f t)
   bimap _ g (N n) = N (g n)
