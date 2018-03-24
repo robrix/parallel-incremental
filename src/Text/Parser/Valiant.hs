c72390a351dce3a5949b2b79280902aee2040315
Rob Rix <rob.rix@me.com>
2018-03-24 11:18:00 -0400

    Define a Show instance for CFG.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -2,6 +2,7 @@
 module Text.Parser.Valiant where
 
 import Data.Bifunctor
+import Data.Functor.Classes
 import Data.Monoid
 
 data BiNF s = U s | B (BiNF s) (BiNF s)
@@ -12,6 +13,12 @@ data CFG f t n = CFG { start :: n, rules :: [(n, f (Symbol t n))] }
 
 size :: (Foldable f, Num a) => CFG f t n -> a
 size = getSum . foldMap (fromIntegral . length . snd) . rules
+
+instance (Show1 f, Show t, Show n) => Show (CFG f t n) where
+  showsPrec d (CFG s r) = showParen (d > 0) $ showString "CFG { start = " . showsPrec 0 s . showString ", rules" . liftShowsPrec (liftShowsPrec showsPrec1 showList1) (liftShowList showsPrec1 showList1) 0 r . showString " }"
+    where showList1 :: (Show1 g, Show a) => [g a] -> ShowS
+          showList1 = liftShowList showsPrec showList
+
 
 data Symbol t n = T t | N n
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
