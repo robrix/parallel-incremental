8b7a88af0f173e356a11b24b686fa4cf9321d485
Rob Rix <rob.rix@me.com>
2018-03-24 10:39:34 -0400

    Define a constructor taking a predicate.

--- src/Data/Relation.hs
+++ src/Data/Relation.hs
@@ -2,10 +2,16 @@
 module Data.Relation
 ( Relation
 , fromRelation
+, fromPredicate
 ) where
+
+import Control.Monad
 
 newtype Relation i a = Relation (i -> Maybe a)
   deriving (Functor)
 
 fromRelation :: (i -> Maybe a) -> Relation i a
 fromRelation = Relation
+
+fromPredicate :: (i -> Bool) -> Relation i i
+fromPredicate predicate = Relation (\ i -> guard (predicate i) *> pure i)
