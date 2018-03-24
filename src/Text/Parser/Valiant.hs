510838adbf10ed3acc94cf886bf87bf71b4f0d94
Rob Rix <rob.rix@me.com>
2018-03-24 11:04:02 -0400

    Compute the size of a CFG.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -1,10 +1,14 @@
 module Text.Parser.Valiant where
 
 import Data.Bifunctor
+import Data.Monoid
 
 data TwoNF t = U t | B (TwoNF t) (TwoNF t)
 
 data CFG t n = CFG { start :: n, rules :: [(n, [Production t n])] }
+
+size :: Num a => CFG t n -> a
+size = getSum . foldMap (fromIntegral . length . snd) . rules
 
 data Production t n = T t | N n
 
