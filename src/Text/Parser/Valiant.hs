2e1bb3c8f7d7749c402c698ecad7737a82372ebb
Rob Rix <rob.rix@me.com>
2018-03-24 11:13:31 -0400

    Derive a few instances.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -1,16 +1,20 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable #-}
 module Text.Parser.Valiant where
 
 import Data.Bifunctor
 import Data.Monoid
 
 data BiNF s = U s | B (BiNF s) (BiNF s)
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 data CFG f t n = CFG { start :: n, rules :: [(n, f (Symbol t n))] }
+  deriving (Foldable, Functor, Traversable)
 
 size :: (Foldable f, Num a) => CFG f t n -> a
 size = getSum . foldMap (fromIntegral . length . snd) . rules
 
 data Symbol t n = T t | N n
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
 
 symbol :: (t -> a) -> (n -> a) -> Symbol t n -> a
 symbol f _ (T t) = f t
