2984337ccef3635651b7b30805979c8cf01dba44
Rob Rix <rob.rix@me.com>
2018-03-24 10:38:17 -0400

    Define a constructor taking a function i -> Maybe a.

--- src/Data/Relation.hs
+++ src/Data/Relation.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Data.Relation
 ( Relation
+, fromRelation
 ) where
 
 newtype Relation i a = Relation (i -> Maybe a)
   deriving (Functor)
+
+fromRelation :: (i -> Maybe a) -> Relation i a
+fromRelation = Relation
