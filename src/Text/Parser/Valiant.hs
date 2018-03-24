cfb83c14d96a64f4c7e05c28828ba7293bed462a
Rob Rix <rob.rix@me.com>
2018-03-24 11:05:31 -0400

    Define an eliminator for Symbol.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -12,6 +12,9 @@ size = getSum . foldMap (fromIntegral . length . snd) . rules
 
 data Symbol t n = T t | N n
 
+symbol :: (t -> a) -> (n -> a) -> Symbol t n -> a
+symbol f _ (T t) = f t
+symbol _ g (N n) = g n
+
 instance Bifunctor Symbol where
-  bimap f _ (T t) = T (f t)
-  bimap _ g (N n) = N (g n)
+  bimap f g = symbol (T . f) (N . g)
