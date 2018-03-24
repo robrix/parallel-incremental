b9f06dad8f04cd2ba674d8d6be8a15ddc8ce79b4
Rob Rix <rob.rix@me.com>
2018-03-24 10:58:50 -0400

    Productions are bifunctors.

--- src/Text/Parser/Valiant.hs
+++ src/Text/Parser/Valiant.hs
@@ -1,5 +1,11 @@
 module Text.Parser.Valiant where
 
+import Data.Bifunctor
+
 data TwoNF t = U t | B (TwoNF t) (TwoNF t)
 
 data Production t n = T t | N n
+
+instance Bifunctor Production where
+  bimap f _ (T t) = T (f t)
+  bimap _ g (N n) = N (g n)
